c1b6586ed9d5a30dbed942b58b0d8ab89b4abae9
SiddhantDeval <siddhantdeval15@gmail.com>
2025-10-21 23:47:34 +0530

    add prisma, configure development package scripts cmd

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,14 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
+// Determine the environment
+const env = process.env.NODE_ENV || 'development'
+
+// Load the corresponding .env file
+dotenv.config({ path: `.env.${env}` })
 
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 app.get('/', (req, res) => {
     res.send('Hello, TypeScript with Express!')
