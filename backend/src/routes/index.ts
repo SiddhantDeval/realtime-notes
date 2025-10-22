e5bf21dfcc6ae9a1303378ac0daf949d58aae9d3
SiddhantDeval <siddhantdeval15@gmail.com>
2025-10-23 00:44:37 +0530

    Added not found route response

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import authRoutes from '@/routes/authRoute'
+import { ResponseHelper } from '@/helpers/responseHelper'
 
 const router = express.Router()
 router.use('/auth', authRoutes)
+router.use((req, res) => ResponseHelper.notFound(res))
 
 export default router
